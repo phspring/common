c4a0143a8b4348a61e9f81e5eb8b89422692b064
phspring <>
2014-01-28 17:41:06 +0100

    Added Config class to initialize the annotation searc path ( Doctrine requirement )

---
+++ lib/PhSpring/Engine/Config.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace PhSpring\Engine;
+
+/**
+ * Description of Config
+ *
+ * @author lobiferi
+ */
+class Config {
+    public static function init() {
+        $path = str_replace(str_replace('\\', DIRECTORY_SEPARATOR, __NAMESPACE__), '', __DIR__);
+        Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespace("PhSpring\Annotations", $path);
+    }
+}
