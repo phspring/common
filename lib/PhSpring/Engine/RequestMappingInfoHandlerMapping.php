4b6aa3420dc899fb76fcaaa3804b1e7b032d7096
PhSpring <info@phspring.nl>
2015-02-11 10:14:20 +0100

    Bugfix

--- lib/PhSpring/Engine/RequestMappingInfoHandlerMapping.php
+++ lib/PhSpring/Engine/RequestMappingInfoHandlerMapping.php
@@ -1,5 +1,5 @@
 <?php
-
+die('This is not working!! This is temporally!! '. __FILE__);
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
