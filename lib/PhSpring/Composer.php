09b6b55a62b830eea721cb08148c205e0875dc85
PhSpring <info@phspring.nl>
2015-02-14 01:51:34 +0100

    Bugfix

--- lib/PhSpring/Composer.php
+++ lib/PhSpring/Composer.php
@@ -17,8 +17,8 @@ class Composer {
     public static function addAnnotationNamespaces( \Composer\Script\Event $event){
         $annotationNamespaces = array();
         $annotationNamespaces['PhSpring\Annotations'] = dirname(dirname(dirname((new \ReflectionClass('PhSpring\Annotations\Config'))->getFileName())));
-        $annotationNamespaces['Symfony\Component\Validator'] = dirname(dirname(dirname(dirname((new \ReflectionClass('Symfony\Component\Validator\Validator'))->getFileName()))));
-        $binDir = dirname(dirname($annotationNamespaces['Symfony\Component\Validator'])).'/bin/phspring';
+        $annotationNamespaces['Symfony\Component\Validator\Validator\RecursiveValidator'] = dirname(dirname(dirname(dirname((new \ReflectionClass('Symfony\Component\Validator\Validator\RecursiveValidator\Validator'))->getFileName()))));
+        $binDir = dirname(dirname($annotationNamespaces['Symfony\Component\Validator\Validator\RecursiveValidator'])).'/bin/phspring';
         if(!is_dir($binDir)){
             mkdir($binDir);
         }
